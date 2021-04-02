b60056f11e68fadc730e7c399085705f8a8e3430
Hoistyr <hoistyd@gmail.com>
2021-04-01 19:22:40 -0600

    Removed api key

--- src/weatherData.js
+++ src/weatherData.js
@@ -1,7 +1,7 @@
 const getData = async(cityName, state, country) => {
   try {
     console.log('in try');
-    const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${state || ''},${country || ''}&units=imperial&id=524901&appid=52820e92985ad85f149530d6c4d05300`, {mode: 'cors'});
+    const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${state || ''},${country || ''}&units=imperial&id=524901&appid=API_KEY`, {mode: 'cors'});
 
     const weatherData = await weatherResponse.json();
     
